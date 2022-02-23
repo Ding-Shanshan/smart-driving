37a31fd56ac569b8ebfb74e9ade0066d7fb58d3e
吕佳琦 <2802935725@qq.com>
2022-02-23 11:24:17 +0800

    增加定时器，在点击运行后调用（怎么插值到页面还在整）

--- src/components/printResult.vue
+++ src/components/printResult.vue
@@ -1,23 +1,67 @@
 <template>
   <div>
     <h4>运行结果</h4>
+    <h4 id=mytime>{{runtime}}</h4>
   </div>
 </template>
 
 <script>
+
 export default {
+  props:['isRun'],
   setup() {
     
   },
+  data(){
+      return{
+        runTime:'',
+        runtime:''
+      }
+    },
+  watch:{
+    isRun(newValue,oldValue){
+      console.log(newValue,oldValue);
+      if(newValue === true && oldValue ===false){
+        var minute,second;
+        minute=second=0;
+        var millisecond=0;
+        var int;
+        //开始函数
+        int=setInterval(timer,50);
+        //计时函数
+        function timer()
+        {
+          millisecond=millisecond+50;
+          if(millisecond>=1000)
+            {
+              millisecond=0;
+              second=second+1;
+            }
+          if(second>=60)
+            {
+              second=0;
+              minute=minute+1;
+            }
+          let runtime = minute+'分'+second+'秒'+millisecond+'毫秒'
+          console.log(runtime)
+        }
+        //重置函数
+        // function Reset()
+        // {
+        //   window.clearInterval(int);
+        //   millisecond=hour=minute=second=0;
+        //   '00分00秒000毫秒';
+        // }
+        // //暂停函数
+        // function stop()
+        // {
+        //   window.clearInterval(int);
+        // }
+          }
+        }
+      },
+  methods:{
+
+  }
 }
 </script>
-
-<style lang="less" scoped>
-h4 {
-    text-align: center;
-}
-div {
-    font-size: 14px;
-    font-weight: 400;
-}
-</style>
